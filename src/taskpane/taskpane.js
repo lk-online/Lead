b9fd0ba9bee5fe5ce616f0e5220d6ab5773877c7
Loukas Koropoulis <54578040+lk-online@users.noreply.github.com>
2024-07-06 11:20:34 +0300

    fixed all issues

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -1,8 +1,3 @@
-/*
- * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
- * See LICENSE in the project root for license information.
- */
-
 /* global document, Office, console, setTimeout */
 
 const EMAIL_TEMPLATES = {
@@ -113,7 +108,8 @@ Office.onReady((info) => {
     parentContainer.style.display = "flex";
 
     // Attach click event using event delegation
-    parentContainer.addEventListener("click", (event) => {
+    const buttonContainer = document.getElementById("buttonContainer");
+    buttonContainer.addEventListener("click", (event) => {
       const target = event.target;
 
       switch (target.id) {
@@ -326,8 +322,8 @@ class EmailUtility {
   }
 
   getMessageId() {
-    const messageObject = JSON.stringify(this);
-    console.log(messageObject);
+    const conversationId = this.item.conversationId;
+    return conversationId;
   }
 }
 
